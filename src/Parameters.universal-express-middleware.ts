978f0eeca5afe72c6c6225979f2fbfad369fafda
David De Anda <omarandstuff@gmail.com>
2024-11-29 23:37:04 -0600

    Right middleware each strategy

--- src/Parameters.universal-express-middleware.ts
+++ src/Parameters.universal-express-middleware.ts
@@ -1,12 +1,10 @@
-import { BaseMiddleware, Middleware, MiddlewareStrategy } from '@universal-packages/express-controllers'
+import { BaseMiddleware, Middleware } from '@universal-packages/express-controllers'
 import { injectParameters } from '@universal-packages/express-parameters'
 
 import { CURRENT_OPTIONS } from './express-controllers-parameters'
 
-@Middleware()
+@Middleware({ strategy: 'each' })
 export default class ParametersMiddleware extends BaseMiddleware {
-  public static readonly strategy: MiddlewareStrategy = 'each'
-
   public async middleware(): Promise<void> {
     injectParameters(this.request, CURRENT_OPTIONS.union)
   }
